096ba19de7654ffe62f97c249394dbd7dd532866
User <User@WIN-5U3F60ENH1L>
2022-10-23 00:21:53 +0300

    Добавьте файлы проекта.

---
+++ ot_a_do_20/ot_a_do_20.cpp
@@ -0,0 +1,23 @@
+﻿// ot_a_do_20.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    int a;
+    long long b = 1;//начальное значение
+    cin >> a;// число, которое вводят с клавиатуры
+    if (a == 0)
+        cout << "Ошибка";
+
+    for (int i= a;i<=20;i++)
+    {
+        b *= i;
+        cout << b << endl;
+    }
+    
+
+}
+
